2dd25d8d6ad96edc2662c6f599c15e4dcc94873f
William Murphy <will.murphy1994@gmail.com>
2018-10-30 11:34:46 -0700

    Add skeleton for HPTracker tests

---
+++ src/tests/HPTracker.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+test('HP tracker renders properly', () => {
+    const component = renderer.create(
+        // Create DiceRoller component here
+    )
+});
