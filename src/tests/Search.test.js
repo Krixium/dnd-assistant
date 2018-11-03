0da3e491013d41b06f3c778436c98835c7419a86
William Murphy <will.murphy1994@gmail.com>
2018-11-02 17:48:08 -0700

    Add search test skeleton

---
+++ src/tests/Search.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render, fireEvent, getByPlaceholderText} from 'react-testing-library';
+import SearchController from '../search/controller/SearchController';
+
+it('renders nothing', () => {
+    const {getByText, getBySelectText, getByValue} = render(<SearchController />)
+    
+    getByValue("races").value = "Races";
+    getByPlaceholderText("Text").value = "Elf";
+});
