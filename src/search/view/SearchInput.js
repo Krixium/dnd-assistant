9fff36ed67fedabbc13b1d65fe5e4be172f6841a
Haley <haley1652@hotmail.com>
2018-11-15 11:32:46 -0800

    updated css

--- src/search/view/SearchInput.js
+++ src/search/view/SearchInput.js
@@ -4,19 +4,25 @@ class SearchInput extends Component {
   render() {
     return (
       <div>
-        <form>
-          <select name='category' onChange={this.props.categoryChangeCallback}>
-            <option value=''/>
-            <option value='races'>Race</option>
-            <option value='classes'>Class</option>
-            <option value='spells'>Spells</option>
-            <option value='features'>Features</option>
-            <option value='equipment'>Equipment</option>
-            <option value='monsters'>Monsters</option>
-          </select>
-          <input name='value' type='text' onChange={this.props.valueChangeCallback} />
-        </form>
-        <button type='button' onClick={this.props.searchCallback}>Search</button>
+        <div className="floatCenter">
+          <div className="halfWidth">
+            <select name='category' onChange={this.props.categoryChangeCallback}>
+              <option value=''/>
+              <option value='races'>Race</option>
+              <option value='classes'>Class</option>
+              <option value='spells'>Spells</option>
+              <option value='features'>Features</option>
+              <option value='equipment'>Equipment</option>
+              <option value='monsters'>Monsters</option>
+            </select>
+          </div>
+          <div className="halfWidth">
+            <input name='value' type='text' onChange={this.props.valueChangeCallback} />
+          </div>
+        </div>
+        <div className="overflowBlock">
+          <button type='button' onClick={this.props.searchCallback}>Search</button>
+        </div>
       </div>
     );
   }
